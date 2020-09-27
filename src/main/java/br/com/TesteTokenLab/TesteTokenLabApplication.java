eba639c33d0b5c601ea4f34bf9c1cf0e3a465a28
Lucas Roberto Denipotti <lucasrdeni@hotmail.com>
2020-09-27 16:46:35 -0300

    Add files via upload

---
+++ src/main/java/br/com/TesteTokenLab/TesteTokenLabApplication.java
@@ -0,0 +1,13 @@
+package br.com.TesteTokenLab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteTokenLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteTokenLabApplication.class, args);
+	}
+
+}
